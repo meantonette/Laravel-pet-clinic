691b3e9d2c5c0dfb0bab4ef026145921a8cdf7f3
IamGab <gabrielarafolmendoza25@yahoo.com>
2022-02-17 20:54:03 +0800

    Add animal migration

---
+++ database/migrations/2022_02_17_115712_create_animals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->increments('animals_id');
+            $table->string(column: 'animal_name');
+            $table->integer(column: 'age');
+            $table->string(column: 'gender');
+            $table->string(column: 'type');
+            $table->string(column: 'animal_pic');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+};
