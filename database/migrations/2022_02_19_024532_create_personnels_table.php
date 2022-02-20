245e61172e2ab54189760a851f211316d5268c7e
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:14:25 +0800

    Add personnel migration

---
+++ database/migrations/2022_02_19_024532_create_personnels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('personnels', function (Blueprint $table) {
+            $table->increments('personnel_id');
+            $table->string(column: 'full_name');
+            $table->string(column: 'email');
+            $table->string(column: 'password');
+            $table->string(column: 'role');
+            $table->string(column: 'images');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('personnels');
+    }
+};
