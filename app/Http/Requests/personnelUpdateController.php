cefa9102c63a075dddb43113f466277097957a42
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:04:09 +0800

    Add validation in return in personnelUpdateRequest

--- app/Http/Requests/personnelUpdateController.php
+++ app/Http/Requests/personnelUpdateController.php
@@ -15,3 +15,20 @@ class personnelUpdateController extends FormRequest
     {
         return true;
     }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'full_name' => 'required|min:5',
+            'email' => 'required',
+            'password' => 'required|min:5',
+            'role' => 'required|min:5',
+            'images' => 'required|mimes:jpg,png,jpeg,gif|max:5048',
+        ];
+    }
+}
