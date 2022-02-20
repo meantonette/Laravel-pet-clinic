53b681797df607672c0b069af0f845f6bc4b220c
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:12:02 +0800

    Add validation rules in personnel module

---
+++ app/Models/Personnel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Personnel extends Model
+{
+
+    public const VALIDATION_RULES = [
+        'full_name' => [
+            'required',
+            'min:5',
+        ],
+        'email' => [
+            'required',
+            'unique:personnels',
+        ],
+        'password' => [
+            'required',
+            'min:5',
+        ],
+        'role' => [
+            'required',
+            'alpha',
+        ],
+        'images' => [
+            'required',
+            'image',
+            'mimes:jpg,png,jpeg,gif',
+            'max:5048',
+        ],
+    ];
+
