c9abb34b0267911d2383708d6595d58f3bb77669
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:04:34 +0800

    Change true from false in rescuerRequest

---
+++ app/Http/Requests/rescuerRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\Rescuer;
+use Illuminate\Foundation\Http\FormRequest;
+
+class rescuerRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
