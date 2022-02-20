b69f59de2bcf531d566e54a1bacc5f6aa56e80ea
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:22:40 +0800

    Add edit html commands in adopter

---
+++ resources/views/adopters/edit.blade.php
@@ -0,0 +1,66 @@
+@extends('home')
+
+@section('contents')
+<div class="pb-20 my-5">
+    <div class="text-center">
+        <h1 class="text-5xl">
+            Update Adopter
+        </h1>
+    </div>
+<div>
+        <div class="flex justify-center pt-4">
+            <form action="/adopter/{{ $adopters->adopter_id }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="block">
+                    <div>
+                    <label for="first_name" class="text-lg">First Name</label>
+                    <input type="text"
+                    class="block shadow-5xl p-2 my-5 w-full"
+                    name="first_name"
+                    value="{{ $adopters->first_name }}">
+                    @if($errors->has('first_name'))
+                    <p class="text-center text-red-500">{{ $errors->first('first_name') }}</p>
+                    @endif 
+                    </div>
+
+                    <div>
+                    <label for="last_name" class="text-lg">Last_name</label>
+                    <input type="text"
+                    class="block shadow-5xl p-2 my-5 w-full"
+                    name="last_name"
+                    value="{{ $adopters->last_name }}">
+                    @if($errors->has('last_name'))
+                    <p class="text-center text-red-500">{{ $errors->first('last_name') }}</p>
+                    @endif 
+                    </div>
+
+                    <div>
+                    <label for="phone_number" class="text-lg">Phone Number</label>
+                    <input type="text"
+                    class="block shadow-5xl p-2 my-5 w-full"
+                    name="phone_number"
+                    value="{{ $adopters->phone_number }}">
+                    @if($errors->has('phone_number'))
+                    <p class="text-center text-red-500">{{ $errors->first('phone_number') }}</p>
+                    @endif 
+                    </div>
+
+                    <div>
+                    <label for="images" class="text-lg">Adopter Pic</label>
+                    <input type="file"
+                    class="block shadow-5xl p-2 w-full"
+                    name="images">
+                    <img src="{{ asset('uploads/adopters/'.$adopters->images)}}" alt="I am A Pic" width="100" height="100" class="ml-24 pb-2">
+                    @if($errors->has('images'))
+                    <p class="text-center text-red-500">{{ $errors->first('images') }}</p>
+                    @endif 
+                    </div>
+
+                    <label for="animals_id" class="text-lg">Animal Name</label>
+                    <select name="animals_id" id="animals_id" class="block shadow-5xl p-2 w-full">
+                        @foreach ($animals as $animal)
+                            <option value="{{ $animal->animals_id }}">{{ $animal->animal_name }}</option>
+                        @endforeach
+                    </select>
+
