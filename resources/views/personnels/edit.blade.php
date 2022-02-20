0f20b1bc5633b83428ef41d3322d0fd107895660
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:31:50 +0800

    Add edit html commands in personnel

---
+++ resources/views/personnels/edit.blade.php
@@ -0,0 +1,71 @@
+@extends('home')
+
+@section('contents')
+<div class="pb-20 my-5">
+    <div class="text-center">
+        <h1 class="text-5xl">
+            Update Personnel
+        </h1>
+    </div>
+<div>
+        <div class="flex justify-center pt-4">
+            <form action="/personnel/{{ $personnels->personnel_id }}" method="POST" enctype="multipart/form-data">
+                @csrf
+                @method('PUT')
+                <div class="block">
+                    <div>
+                    <label for="full_name" class="text-lg">Full Name</label>
+                    <input type="text"
+                    class="block shadow-5xl p-2 my-5 w-full"
+                    name="full_name"
+                    value="{{ $personnels->full_name }}">
+                    @if($errors->has('full_name'))
+                    <p class="text-center text-red-500">{{ $errors->first('full_name') }}</p>
+                    @endif 
+                    </div>
+
+                    <div>
+                    <label for="email" class="text-lg">Email</label>
+                    <input type="text"
+                    class="block shadow-5xl p-2 my-5 w-full"
+                    name="email"
+                    value="{{ $personnels->email }}">
+                    @if($errors->has('email'))
+                    <p class="text-center text-red-500">{{ $errors->first('email') }}</p>
+                    @endif 
+                    </div>
+
+                    <div>
+                    <label for="password" class="text-lg">Password</label>
+                    <input type="password"
+                    class="block shadow-5xl p-2 my-5 w-full"
+                    name="password"
+                    value="{{old('password')}}">
+                    @if($errors->has('password'))
+                    <p class="text-center text-red-500">{{ $errors->first('password') }}</p>
+                    @endif 
+                    </div>
+
+                    <div>
+                    <label for="role" class="text-lg">Pick Your Role</label>
+                    <select name="role" class="block shadow-5xl p-2 my-5 w-full" value="{{ $personnels->role }}">
+                      <option>Employee</option>
+                      <option>Veterinarian</option>
+                      <option>Volunteer</option>
+                    </select>
+                    @if($errors->has('role'))
+                    <p class="text-center text-red-500">{{ $errors->first('role') }}</p>
+                    @endif 
+                    </div>
+
+                    <div>
+                    <label for="images" class="text-lg">Personnel Pic</label>
+                    <input type="file"
+                    class="block shadow-5xl p-2 w-full"
+                    name="images">
+                    <img src="{{ asset('uploads/personnels/'.$personnels->images)}}" alt="I am A Pic" width="100" height="100" class="ml-24 pb-2">
+                    @if($errors->has('images'))
+                    <p class="text-center text-red-500">{{ $errors->first('images') }}</p>
+                    @endif 
+                    </div>
+
