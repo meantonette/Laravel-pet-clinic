2ea773c5ce772ebd8b988027643ea28926554d2d
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 21:54:45 +0800

    Import files that will be call in  the controller

---
+++ app/Http/Controllers/rescuerController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\rescuerRequest;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\File;
+use App\Models\Rescuer;
+
