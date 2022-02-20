e5e0d912ed67f5e3ba164b6a2d90a9e0ec21b504
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 21:15:38 +0800

    Add adopter index function

--- app/Http/Controllers/adopterController.php
+++ app/Http/Controllers/adopterController.php
@@ -8,3 +8,19 @@ use Illuminate\Support\Facades\File;
 use App\Http\Requests\adopterRequest;
 use App\Models\Animal;
 use App\Models\Adopter;
+
+class adopterController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $adopters = Adopter::all();
+        return view('adopters.index',[
+            'adopters' => $adopters
+        ]);
+    }
+
