df09af0aedc7e14d77486ec574022a30cca47bcd
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 21:55:23 +0800

    Add rescuer create function

--- app/Http/Controllers/rescuerController.php
+++ app/Http/Controllers/rescuerController.php
@@ -23,3 +23,13 @@ class rescuerController extends Controller
         ]);
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('rescuers.create');
+    }
+
