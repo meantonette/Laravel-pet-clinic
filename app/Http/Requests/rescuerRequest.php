22b855cf16b45c5099e8f760fa3387619b843543
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:05:01 +0800

    Add validation rules in return in rescuerRequest

--- app/Http/Requests/rescuerRequest.php
+++ app/Http/Requests/rescuerRequest.php
@@ -17,3 +17,13 @@ class rescuerRequest extends FormRequest
         return true;
     }
 
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return Rescuer::VALIDATION_RULES;
+    }
+}
