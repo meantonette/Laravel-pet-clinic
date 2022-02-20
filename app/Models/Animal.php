0a6cf612f58b11b7c74528b2edc312900cd11589
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:07:22 +0800

    Add validation rules in animal module

--- app/Models/Animal.php
+++ app/Models/Animal.php
@@ -7,6 +7,35 @@ use Illuminate\Database\Eloquent\Model;
 
 class Animal extends Model
 {
+
+    public const VALIDATION_RULES = [
+        'animal_name' => [
+            'required',
+            'alpha',
+            'min:3',
+        ],
+        'age' => [
+            'required',
+            'numeric',
+            'min:1',
+            'max:20',
+        ],
+        'gender' => [
+            'required',
+            'alpha',
+        ],
+        'type' => [
+            'required',
+            'alpha',
+        ],
+        'images' => [
+            'required',
+            'image',
+            'mimes:jpg,png,jpeg,gif',
+            'max:5048',
+        ],
+    ];
+
     use HasFactory;
 
     protected $table = 'animals';
