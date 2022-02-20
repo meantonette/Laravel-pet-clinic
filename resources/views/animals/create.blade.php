1671fb678181bfcb8a5fcb4f8fd839f4ada2a36e
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:24:29 +0800

    Add create html commands in animals

--- resources/views/animals/create.blade.php
+++ resources/views/animals/create.blade.php
@@ -4,7 +4,7 @@
 <div class="pb-20 my-5">
     <div class="text-center">
         <h1 class="text-5xl">
-            Create Animal
+            Add Animal
         </h1>
     </div>
 <div>
@@ -13,34 +13,71 @@
             <form action="/animals" method="POST" enctype="multipart/form-data">
                 @csrf
                 <div class="block">
+                    <div>
                     <label for="animal_name" class="text-lg">Animal Name</label>
                     <input type="text"
                     class="block shadow-5xl p-2 my-5 w-full"
                     name="animal_name"
-                    placeholder="Animal Name">
+                    placeholder="Animal Name"
+                    value="{{old('animal_name')}}">
+                    @if($errors->has('animal_name'))
+                    <p class="text-center text-red-500">{{ $errors->first('animal_name') }}</p>
+                    @endif 
+                    </div>
 
+                    <div>
                     <label for="age" class="text-lg">Age</label>
                     <input type="text"
                     class="block shadow-5xl p-2 my-5 w-full"
                     name="age"
-                    placeholder="Age">
+                    placeholder="Age"
+                    value="{{old('age')}}">
+                    @if($errors->has('age'))
+                    <p class="text-center text-red-500">{{ $errors->first('age') }}</p>
+                    @endif 
+                    </div>
 
+                    <div>
                     <label for="gender" class="text-lg"r">Gender</label>
                     <input type="text"
                     class="block shadow-5xl p-2 my-5 w-full"
                     name="gender"
-                    placeholder="Gender">
+                    placeholder="Gender"
+                    value="{{old('gender')}}">
+                    @if($errors->has('gender'))
+                    <p class="text-center text-red-500">{{ $errors->first('gender') }}</p>
+                    @endif 
+                    </div>
 
+                    <div>
                     <label for="type" class="text-lg"r">Type</label>
                     <input type="text"
                     class="block shadow-5xl p-2 my-5 w-full"
                     name="type"
-                    placeholder="Type of Animal">
+                    placeholder="Type of Animal"
+                    value="{{old('type')}}">
+                    @if($errors->has('type'))
+                    <p class="text-center text-red-500">{{ $errors->first('type') }}</p>
+                    @endif 
+                    </div>
 
-                    <label for="animal_pic"" class="text-lg">Animal Pic</label>
+                    <div>
+                    <label for="images" class="text-lg">Animal Pic</label>
                     <input type="file"
                     class="block shadow-5xl p-2 w-full"
-                    name="animal_pic">
+                    name="images"
+                    value="{{old('images')}}">
+                    @if($errors->has('images'))
+                    <p class="text-center text-red-500">{{ $errors->first('images') }}</p>
+                    @endif 
+                    </div>
+
+                    <label for="rescuer_id"" class="text-lg">Rescuer</label>
+                    <select name="rescuer_id" id="rescuer_id" class="block shadow-5xl p-2 w-full">
+                        @foreach ($rescuers as $rescuer)
+                            <option value="{{ $rescuer->rescuer_id }}">{{ $rescuer->last_name }},{{ $rescuer->first_name }}</option>
+                        @endforeach
+                    </select>
 
                     <button type="submit" class="bg-gray-800 text-white block p-2 mt-5 w-full font-bold">
                         Submit
