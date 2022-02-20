570e55892147822194882fb2aafdb59db7149147
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 21:52:18 +0800

    Add personnel create function

--- app/Http/Controllers/personnelController.php
+++ app/Http/Controllers/personnelController.php
@@ -72,3 +72,13 @@ class personnelController extends Controller
     }
 
     /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('personnels.create');
+    }
+
+    /**
