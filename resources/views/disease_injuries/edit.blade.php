c5c3fddeef92a43d8446257d7c98a8598015efbf
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:27:51 +0800

    Add buttons in diseaseInjury

--- resources/views/disease_injuries/edit.blade.php
+++ resources/views/disease_injuries/edit.blade.php
@@ -31,3 +31,14 @@
                             <option value="{{ $animal->animals_id }}">{{ $animal->animal_name }}</option>
                         @endforeach
                     </select>
+
+                    <div class="grid grid-cols-2 gap-2 w-full">
+                        <button type="submit" class="bg-green-800 text-white font-bold p-2 mt-5">
+                            Submit
+                        </button>
+                        <a href="{{url()->previous()}}" class="bg-gray-800 text-white font-bold p-2 mt-5 text-center" role="button">Cancel</a>
+                        </div>
+                </div>
+            </form>
+        </div>
+@endsection
