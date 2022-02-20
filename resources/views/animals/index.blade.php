9e2e9c253bc90eb1ea5f4953662889a32e356264
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:26:09 +0800

    Add message per successful session in animals

--- resources/views/animals/index.blade.php
+++ resources/views/animals/index.blade.php
@@ -2,6 +2,20 @@
 
 @section('contents')
 
+@if ($message = Session::get('add'))
+ <div class="bg-red-500 p-4">
+    <strong class="text-white text-3xl pl-4">{{ $message }}</strong>
+ </div>
+ @elseif ($message = Session::get('update'))
+ <div class="bg-red-500 p-4">
+    <strong class="text-white text-3xl pl-4">{{ $message }}</strong>
+ </div>
+ @elseif ($message = Session::get('delete'))
+ <div class="bg-red-500 p-4">
+    <strong class="text-white text-3xl pl-4">{{ $message }}</strong>
+ </div>
+@endif
+
 <div class="pt-8 pb-4 px-8">
     <a href="animals/create" class="p-3 border-none italic text-white bg-black text-lg">
         Add a new animal &rarr;
