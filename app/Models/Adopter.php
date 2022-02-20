656f0e61da9153de4182d5beb6440de022fa2136
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:05:52 +0800

    Add validation rules in adopter module

---
+++ app/Models/Adopter.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Adopter extends Model
+{
+
+    public const VALIDATION_RULES = [
+        'first_name' => [
+            'required',
+            'alpha',
+            'min:3',
+        ],
+        'last_name' => [
+            'required',
+            'alpha',
+            'min:3',
+        ],
+        'phone_number' => [
+            'required',
+            'numeric',
+        ],
+        'images' => [
+            'required',
+            'image',
+            'mimes:jpg,png,jpeg,gif',
+            'max:5048',
+        ],
+    ];
