fcacdfd882cbe48c45c5b81bfdaf330d684c2b04
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 22:30:34 +0800

    Add dashboard html commands in personnel

---
+++ resources/views/personnels/dashboard.blade.php
@@ -0,0 +1,29 @@
+@extends('home')
+
+@section('contents')
+
+<h1 class="text-center text-5xl pb-8 text-blue-600">Welcome To ACME Pet Clinic, {{ $personnel->full_name }}</h1>
+<hr>
+<div class="py-3">
+    <table class="table-auto">
+        <tr class="text-white"> 
+            <th class="w-screen text-3xl">Id</th>
+            <th class="w-screen text-3xl">Full Name</th>
+            <th class="w-screen text-3xl">Email</th>
+            <th class="w-screen text-3xl">Personnel Pic</th>
+            <th class="w-screen text-3xl">Action</th>
+        </tr>
+
+      <tr>
+        <td class=" text-center text-3xl">
+            {{ $personnel->personnel_id }}
+        </td>
+        <td class=" text-center text-3xl">
+            {{ $personnel->full_name }}
+        </td>
+          <td class=" text-center text-3xl">
+                {{ $personnel->email }}
+          </td>
+          <td class="pl-32">
+            <img src="{{ asset('uploads/personnels/'.$personnel->images)}}" alt="I am A Pic" width="75" height="75">
+          </td>
