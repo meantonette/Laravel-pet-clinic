5631857d4521bdbdad45ee5e627a4c0fa772c778
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 21:15:08 +0800

    Import files that will be call in  the controller

---
+++ app/Http/Controllers/adopterController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\File;
+use App\Http\Requests\adopterRequest;
+use App\Models\Animal;
+use App\Models\Adopter;
