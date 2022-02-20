645b9f5a3bad053fe8690a440570be860bbbc7c6
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 21:35:39 +0800

    mport files that will be call in  the controller

---
+++ app/Http/Controllers/personnelController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests\personnelRequest;
+use App\Http\Requests\personnelUpdateController;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\Personnel;
