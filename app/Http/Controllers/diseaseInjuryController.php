0361a93015dfcdc1dcb41d9f988f4619a615ea99
IamGab <gabrielarafolmendoza25@gmail.com>
2022-02-20 21:32:48 +0800

    Import files that will be call in  the controller

---
+++ app/Http/Controllers/diseaseInjuryController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Facades\File;
+use App\Http\Requests\diseaseInjuryRequest;
+use App\Models\DiseaseInjury;
+use App\Models\Animal;
