106871e6aa46049c3221ab8666fddbd1cf91a5e5
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-02-25 22:14:15 +0800

    Add soft delete in personnel

--- app/Models/Personnel.php
+++ app/Models/Personnel.php
@@ -4,7 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Personnel extends Model
 {
 
@@ -35,7 +35,9 @@ class Personnel extends Model
 
     use HasFactory;
 
-    protected $table = 'personnels';
+    use SoftDeletes;
+
+    protected $dates = ["deleted_at"];
 
     protected $primaryKey = 'personnel_id';
 
