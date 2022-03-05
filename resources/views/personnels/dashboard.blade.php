a74e6a4c353ceb2128fed1667618cfed9d8e2cf8
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-03-05 13:00:54 +0800

    Update padding top

--- resources/views/personnels/dashboard.blade.php
+++ resources/views/personnels/dashboard.blade.php
@@ -35,7 +35,7 @@
 </div>
 </div>
 <hr>
-<h1 class="text-center text-5xl pt-32 px-4 text-green-600">Our mission is to provide the highest quality animal care to
+<h1 class="text-center text-5xl pt-20 px-4 text-green-600">Our mission is to provide the highest quality animal care to
   your pet and improve his or her quality of life through the preservation, enhancement, and restoration of your pets
   health.</h1>
 
