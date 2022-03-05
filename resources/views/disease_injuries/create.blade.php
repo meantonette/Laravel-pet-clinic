3d0fdf47190d72c033c98446f4ae46b1fa40ab3a
IamGab <gabrielarafol.mendoza@tup.edu.ph>
2022-03-05 17:37:26 +0800

    Update width

--- resources/views/disease_injuries/create.blade.php
+++ resources/views/disease_injuries/create.blade.php
@@ -25,11 +25,12 @@
                     <div class="inline">
                         <label class="block text-lg pb-2">Animals</label>
                         @foreach ($animals as $animals_id => $animal)
-                        {!!Form::label('animals', $animal,array('class'=>'inline-block w-1/12')) !!}
+                        {!!Form::label('animals', $animal,array('class'=>'inline-block w-1/5')) !!}
                         {!! Form::checkbox('animals_id[]',$animals_id, null, array('class'=>'inline-block
-                        w-1/12','id'=>'animals')) !!}
+                        w-1/5','id'=>'animals')) !!}
                         @endforeach
-
+                    </div>
+                    
                         <div class="grid grid-cols-2 gap-2 w-full">
                             <button type="submit" class="bg-green-800 text-white font-bold p-2 mt-5">
                                 Submit
